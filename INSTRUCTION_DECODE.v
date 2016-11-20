bee6a43f0afa14646c2881141024cb0b370f798a
Nari Kan <>
2016-11-21 01:19:09 +0800

    I think I complete beq

--- INSTRUCTION_DECODE.v
+++ INSTRUCTION_DECODE.v
@@ -7,26 +7,32 @@ module INSTRUCTION_DECODE(
 	PC,
 	MW_RD,
 	MW_ALUout,
+	MW_compareFlag,
 
 	A,
 	B,
 	RD,
 	ALUctr,
 	DX_lwFlag,
-	DX_swFlag
+	DX_swFlag,
+	DX_compareFlag,
+	DX_PC
 );
 
 input clk,rst;
 input [31:0]IR, PC, MW_ALUout;
 input [4:0] MW_RD;
+input [2:0] MW_compareFlag;
 
-output reg [31:0] A, B;
+output reg [31:0] A, B, DX_PC;
 output reg [4:0] RD;
 output reg [2:0] ALUctr;
 output reg DX_lwFlag, DX_swFlag;
+output reg [2:0] DX_compareFlag;
 
 //register file
 reg [31:0] REG [0:31];
+
 
 //Write back
 always @(posedge clk)//add new Dst REG source here
@@ -41,10 +47,12 @@ begin
 	if(rst)
 	  begin
 	    A 	<=32'b0;
+		DX_PC <=PC;
 	  end
 	else
 	  begin
 	    A 	<=REG[IR[25:21]];
+		DX_PC <=PC;
 	  end
 end
 
@@ -58,10 +66,11 @@ begin
 		ALUctr 	<=3'b0;
 		DX_lwFlag	<=1'b0;
 		DX_swFlag <=1'b0;
+		DX_compareFlag <=3'd0;
 	  end
 	else
 	  begin
-	    case(IR[31:26])
+		case(IR[31:26])
 		  6'd0://R-Type
 		    begin
 			  case(IR[5:0])//funct & setting ALUctr
@@ -71,6 +80,7 @@ begin
 		            RD	<=IR[15:11];
 								DX_lwFlag <=1'b0;
 								DX_swFlag <=1'b0;
+								DX_compareFlag <=3'd0;
 					ALUctr <=3'd0;//self define ALUctr value
 				  end
 				6'd34://sub
@@ -79,6 +89,7 @@ begin
 								RD	<=IR[15:11];
 								DX_lwFlag <=1'b0;
 								DX_swFlag <=1'b0;
+								DX_compareFlag <=3'd0;
 					//define sub behavior here
 					ALUctr <=3'd1;//self define ALUctr value
 				  end
@@ -89,6 +100,7 @@ begin
 							ALUctr <=3'd2;
 							DX_lwFlag <=1'b0;
 							DX_swFlag <=1'b0;
+							DX_compareFlag <=3'd0; //In ALUctr=2, compareFlag=0->slt
 					//define slt behavior here
 				  end
 			  endcase
@@ -100,6 +112,7 @@ begin
 				ALUctr <=3'd0;
 				DX_lwFlag <=1'b1;
 				DX_swFlag <=1'b0;
+				DX_compareFlag <=3'd0;
 			  //define lw behavior here
 			end
 	      6'd43://sw
@@ -109,10 +122,21 @@ begin
 				ALUctr <=3'd0;
 				DX_lwFlag <=1'b0;
 				DX_swFlag <=1'b1;
+				DX_compareFlag <=3'd0;
 			  //define sw behavior here
 			end
 	      6'd4://beq
 			begin
+				if (IR[15]==1)
+					B <={16'b1111111111111111,IR[15:0]};
+				else
+					B <={16'b0,IR[15:0]};
+				RD <=REG[IR[20:16]];	//Immediate value
+				DX_lwFlag <=1'b0;
+				DX_swFlag <=1'b0;
+				DX_compareFlag <=3'd1;
+				ALUctr <=3'd2;
+
 			  //define beq behavior here
 			end
 	      6'd2://j
